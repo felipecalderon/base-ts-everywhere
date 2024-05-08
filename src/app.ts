7513ae91ff83decb4310f9dd864fc7c6e4c9f357
Felipe <felipe.calderon321@gmail.com>
2024-05-08 10:14:44 -0400

    ajustando prettier indentacion

--- src/app.ts
+++ src/app.ts
@@ -4,25 +4,25 @@ import express, { Response, Request } from 'express'
 
 const app = express()
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_KEY,
+    apiKey: process.env.OPENAI_KEY,
 })
 
 app.use(express.json())
 app.post('/', async (req: Request, res: Response) => {
-  try {
-    // const { role, content } = req.body
-    const fechData = await openai.chat.completions.create({
-      messages: [...instructions],
-      model: 'gpt-4-1106-preview',
-    })
-    const respIA = fechData.choices[0].message
-    return res.json({ respuestaGPT: respIA })
-  } catch (error) {
-    console.log(error)
-    res.json(error)
-  }
+    try {
+        // const { role, content } = req.body
+        const fechData = await openai.chat.completions.create({
+            messages: [...instructions],
+            model: 'gpt-4-1106-preview',
+        })
+        const respIA = fechData.choices[0].message
+        return res.json({ respuestaGPT: respIA })
+    } catch (error) {
+        console.log(error)
+        res.json(error)
+    }
 })
 
 app.listen(3001, () => {
-  console.log('Conectado en express')
+    console.log('Conectado en express')
 })
